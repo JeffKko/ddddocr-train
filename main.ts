e1c279936af19e67df1d29d41d33bb3b21f129f2
Jeff <b3589481400@gmail.com>
2023-04-13 13:54:20 +0800

    first init

---
+++ main.ts
@@ -0,0 +1,75 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+import fs from 'fs';
+
+let count = 0
+
+main()
+
+
+function main() {
+  accessPage()
+}
+
+const sleep = (t: number) => new Promise(r => setTimeout(r, t))
+
+async function accessPage() {
+  const res = await axios.get('https://gen.caca01.com/ttcode/codeking')
+
+  // console.log('res: ', res)
+
+  const $ = cheerio.load(res.data)
+
+  const csrf = $('meta[name="csrf-token"]').attr('content') ?? ''
+
+  console.log(csrf)
+
+
+  test(csrf)
+}
+
+interface CodeItem {
+  ans: string
+  code: string
+  id: number
+}
+
+interface Res {
+  codelist: CodeItem[],
+  ret: number,
+}
+
+async function test(csrf: string) {
+  const { data } = await axios.get<Res>('https://gen.caca01.com/rd/test', {
+    headers: {
+      referer: 'https://gen.caca01.com/ttcode/codeking',
+      'x-csrf-token': csrf,
+      'x-requested-with': 'XMLHttpRequest',
+    }
+  })
+
+  console.log(data)
+
+  const codeList = data.codelist
+
+  for (const obj of codeList) {
+    await sleep(1)
+
+    const timestamp = Date.now()
+
+    fs.writeFileSync(`./images/${obj.ans}_${timestamp}.jpg`, obj.code, { encoding: 'base64' });
+
+    count +=1
+
+    console.log('count: ', count)
+  }
+
+
+  await sleep(2000)
+
+  if (count >= 2000) {
+    return
+  } else {
+    main()
+  }
+}
---
+++ test_single.ts
@@ -0,0 +1,91 @@
+
+import fs from 'fs';
+import Ddddocr from 'ddddocr';
+
+
+
+
+export const useDdddocr = async (
+  path: string,
+  options?: {
+    onnxPath?: string;
+    charsetsPath?: string;
+  },
+) => {
+  try {
+    const t1 = Date.now();
+
+    const init = Ddddocr.create({
+      onnxPath: './models/tixcraftv3_0.6666666666666666_14100_423000_2023-04-13-09-40-09.onnx',
+      charsetsPath: './models/charsets.json',
+    });
+
+    const ddddocr = await init;
+    const captcha = fs.readFileSync(path, {
+      encoding: 'base64',
+    });
+    const result = await ddddocr.classification(Buffer.from(captcha, 'base64'));
+
+    const t2 = Date.now();
+
+    console.log(`驗證碼破解時間: ${(t2 - t1) / 1000} s`);
+
+    return result;
+  } catch (error) {
+    console.log('useDdddocr error', error);
+    throw error;
+  }
+};
+
+// const verifyCode = await useDdddocr(imagePath, {
+//   onnxPath,
+// });
+
+
+const testFolder = './images/';
+
+(async() => {
+  const guessAns = await useDdddocr(`./images/cika_1681328682873.jpg`)
+
+  console.log(guessAns)
+  console.log(guessAns.length)
+})()
+
+// let rightCount = 0
+// let wrongCount = 0
+// let totalCount = 0
+
+// let wrongList: string[] = []
+
+// fs.readdir(testFolder, async(err, filesList) => {
+
+//   for (const file of filesList) {
+//     if (!file.includes('.jpg')) {
+//       continue
+//     }
+//     // console.log(file);
+
+//     const ans = file.split('_')[0]
+
+//     const currentFile = `${testFolder}${file}`
+
+//     totalCount += 1
+
+//     console.log(`${totalCount} ${currentFile}`)
+
+//     const guessAns = await useDdddocr(`./images/${file}`)
+
+//     if (ans === guessAns) {
+//       rightCount += 1
+//     } else {
+//       wrongCount += 1
+
+//       wrongList.push(`${guessAns} ${file}`)
+//     }
+//   }
+
+//   console.log('rightCount: ', rightCount)
+//   console.log('wrongCount: ', wrongCount)
+
+//   fs.writeFileSync(`result_${Date.now()}.txt`, wrongList.join('\n'))
+// });
